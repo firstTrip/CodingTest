48f193be7b2fbe79042bb52ad6f9a10b0594502e
firstTrip <hdnwls961@gmail.com>
2022-01-20 23:30:38 +0900

    42626 Problem

---
+++ 42626/42626/42626/42626.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) 
+{
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    return 0;
+}
