bf9ac5cd3750611e0c36772d3adbbea1ced554e0
firstTrip <hdnwls961@gmail.com>
2022-01-26 23:22:23 +0900

    solve 42587
    use queue

--- 42587/42587/42587/42587.cpp
+++ 42587/42587/42587/42587.cpp
@@ -2,15 +2,44 @@
 
 #include <string>
 #include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    int answer = 0;
-    return answer;
+    queue<int> printer;                         //queue에 index 삽입.
+    vector<int> sorted;                         //정렬된 결과 저장용
+    for (int i = 0; i < priorities.size(); i++) {
+        printer.push(i);
+    }
+    while (!printer.empty()) {
+        int now_index = printer.front();
+        printer.pop();
+        if (priorities[now_index] != *max_element(priorities.begin(), priorities.end())) {
+            //아닌경우 push
+            printer.push(now_index);
+        }
+        else {
+            //맞는경우
+            sorted.push_back(now_index);
+            priorities[now_index] = 0;
+        }
+    }
+    for (int i = 0; i < sorted.size(); i++) {
+        if (sorted[i] == location) return i + 1;
+    }
 }
 
 int main()
 {
+    vector<int> prioritie;
+
+    prioritie = {2,1,3,2};
+
+    solution(prioritie, 2);
+
+    cout<< solution(prioritie, 2);
     return 0;
 }
