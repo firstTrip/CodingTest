9d9a847f642adc1e149efaa811e8f56e8b414247
firstTrip <hdnwls961@gmail.com>
2022-02-01 22:23:17 +0900

    solve 42885

--- 42885/42885/42885/42885.cpp
+++ 42885/42885/42885/42885.cpp
@@ -9,54 +9,33 @@ using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
+    int idx = 0;
     sort(people.begin(), people.end());// people 을 정렬 시킨다.
 
     for(int i=0;i<people.size();i++)
         cout << "people is " << people[i] << endl;
     
-    while (!people.empty())// people 이 빌때까지 돌린다.
+    while (people.size() >idx)
     {
-        for (int i = 0; i < people.size()-1; i++)
+        int back = people.back();
+        people.pop_back();
+
+        if (people[idx] + back <= limit)
         {
-            if (people[i] + people[i + 1] <= limit)
-            {
-                for (int j = i + 1; j < people.size(); j++)
-                {
-                    if (people[i] + people[j] <= limit)
-                    {
-                        cout << "people sum is " << people[i]+ people[j] << endl;
-                        people.erase(people.begin() + i);// 사용한 요소를 삭제한다.
-                        people.erase(people.begin() + j-1);
-
-                        answer++;
-                        break;
-                    }
-
-                }
-                cout << "people is " << people[i] << endl;
-                people.erase(people.begin() + i);
-                answer++;
-            }
-            else
-            {
-                cout << "people is " << people[i] << endl;
-                people.erase(people.begin() + i);
-                answer++;
-            }
-            
+            answer++;
+            idx++;
         }
-        cout << "people size is " << people.size() << endl;
-        cout << "answer is " << answer << endl;
+        else
+            answer++;
     }
-    
-
+   
     return answer;
 }
 
 int main()
 {
     vector<int> people;
-    people = {70,50,80,50};
+    people = {70,50,80,20,30};
 
     solution(people, 100);
 
