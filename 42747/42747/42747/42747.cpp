41188d8b440c88f084f96b3cc2f0a17500f3410c
firstTrip <hdnwls961@gmail.com>
2022-02-08 20:06:08 +0900

    Problem 42747

---
+++ 42747/42747/42747/42747.cpp
@@ -0,0 +1,28 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42747
+// h 값이 최대로 잡고서 계산시에 h 번 이하 남아야 한다?
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    int cnt = 0;
+
+    sort(citations.begin(), citations.end());
+
+
+
+    return answer;
+}
+
+
+
+int main()
+{
+
+    vector<int> cit = {3,0,6,1,5};
+    solution(cit);
+	return 0;
+}
