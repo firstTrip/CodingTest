54074c8b9a16f83475ff45ed05856afcf53549c0
firstTrip <hdnwls961@gmail.com>
2022-02-08 19:17:59 +0900

    solve 49993

--- 49993/49993/49993/49993.cpp
+++ 49993/49993/49993/49993.cpp
@@ -1,47 +1,81 @@
 //https://programmers.co.kr/learn/courses/30/lessons/49993
-
+// 문자열의 시작이 스킬의 기초 스킬이 아닌경우 다 넘기기 
+// 그 이후 검사 
+// it find 이후 그 배열의 숫자 저장 후  C 보다 숫자가 작으면 탈락 바르게 정렬이 되어있는경우에 통과 answer ++
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-bool isIt() {
-
-
-    return false;
-
-    return true;
-}
-
 int solution(string skill, vector<string> skill_trees) {
-    int answer = 0;
-    int idx = 0;
 
-    for (int n = 0; n < skill.length() ; n++)
+    /*
+    vector<string>::iterator it;
+    vector<int> idx;
+
+  
+    for (int i = 0; i < skill_trees.size(); i++)
     {
-        for (int i = 0; i < skill_trees.size(); i++)
+        for (int n = 0; n < skill.length(); n++)
         {
-            for (int j = 0; j < skill_trees[i].length(); j++)
+
+            if (find(skill_trees[i].begin(), skill_trees[i].end(), skill[n]) != skill_trees[i].end())
             {
-                if (skill[i] == skill_trees[i][j])
-                {
-                    idx = j;
-                    
-                    // 앞으로 남은 배열중에 B 말고 이상의 값이 있는지 확인 하기 없으면 answer ++
-                    for (int x = idx; x < skill_trees[i].length(); x++)
-                    {
 
-                    }
-                }
-            
-         
-
+                cout << "skill[n] 은 " << skill[n] << " 있습니당 해당 idx 는 " << find(skill_trees[i].begin(), skill_trees[i].end(), skill[n]) - skill_trees[i].begin() << endl;
+                idx.push_back(find(skill_trees[i].begin(), skill_trees[i].end(), skill[n]) - skill_trees[i].begin());
             }
 
         }
+
+        for (int x = 0; x < idx.size()-1; x++)
+        {
+            if (idx[x] > idx[x + 1])
+            {
+                break;
+
+            }
+            else
+            {
+                if (idx[x + 1] > idx[x + 2])
+                    break;
+                else
+                    answer++;
+            }
+                
+        }
+
+        idx.clear();
     }
-    
+ 
+
+
+    cout << "answer is " << answer << endl;
+    return answer;
+    */
+    int answer = 0;
+    for (int i = 0; i < skill_trees.size(); i++) {
+        vector<char> compare; //최대 비교 변수를 저장 
+
+        for (int k = 0; k < skill_trees[i].size(); k++) {
+            for (int j = 0; j < skill.size(); j++) {
+                if (skill[j] == skill_trees[i][k]) {
+                    char tmp = skill[j]; // 스킬과 스킬 트리 사이의 곂치는 스킬들을 tmp 에 저장
+                    compare.push_back(tmp); // compare 에 순서대로 저장
+                }
+            }
+        }
+        bool check = true;;// check 를 true 로 초기화
+        for (int c = 0; c < compare.size(); c++) {
+            if (skill[c] != compare[c]) {// skill 과 compare 의 서순이 같은지 확인 
+                check = false; // 틀린 경우가 있을경우 check 를 false 로 변경
+            }
+        }
+        if (check)answer++; // 다 같을경우 answer ++
+    }
+    cout << "answer is " << answer << endl;
     return answer;
 }
 
