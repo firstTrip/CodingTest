53a6f9e01e7c8c7681f599fcd28e8ffa26623885
firstTrip <hdnwls961@gmail.com>
2022-02-14 23:57:27 +0900

    Problem 42747

---
+++ 42586/42586/42586/42586.cpp
@@ -0,0 +1,30 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42586
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    stack<int> st;
+
+    while (1)
+    {
+
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> pro = {93,30,55};
+    vector<int> spe = {1,30,5};
+
+    solution(pro,spe);
+
+    return 0;
+}
