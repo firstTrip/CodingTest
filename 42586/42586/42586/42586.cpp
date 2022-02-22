fca394a97470e3906679baea0b17b47ee24191a7
firstTrip <hdnwls961@gmail.com>
2022-02-22 23:07:58 +0900

    Solve 42586

--- 42586/42586/42586/42586.cpp
+++ 42586/42586/42586/42586.cpp
@@ -9,9 +9,21 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    stack<int> st;
 
-    
+    int day;
+    int max_day = 0;
+    for (int i = 0; i < progresses.size(); ++i)
+    {
+        day = (99 - progresses[i]) / speeds[i] + 1; // 소요시간 계산 식 
+
+        if (answer.empty() || max_day < day)
+            answer.push_back(1);
+        else
+            ++answer.back(); //만약 day 보다 max_day 가 작은 경우  answer 의 끝값을 ++ 시켜준다 
+
+        if (max_day < day) // max_day 보다 큰 day 가 있다면 max_day 갱신
+            max_day = day;
+    }
 
     return answer;
 }
