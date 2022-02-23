6f1fffdb3756c2c55d85ce96d5b76c4542b3f691
firstTrip <hdnwls961@gmail.com>
2022-02-24 06:11:42 +0900

    Problem 60057
     문자열 이어 붙이기  https://programmers.co.kr/learn/courses/30/lessons/60057

---
+++ 60057/60057/60057/60057.cpp
@@ -0,0 +1,39 @@
+//https://programmers.co.kr/learn/courses/30/lessons/60057
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int sol(string s , int x)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+
+    }
+}
+
+int solution(string s) {
+    int answer = 0;
+    string tmp = "";
+    int sz = s.length() / 2;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        tmp += s[i];
+
+    }
+
+    return answer;
+}
+
+
+int main()
+{
+    string s = "aabbaccc";
+
+    solution(s);
+
+    return 0;
+}
