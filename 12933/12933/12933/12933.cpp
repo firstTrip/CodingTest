2db08e825ca54cbbe5585d1c09c400d65f8dcd0a
firstTrip <hdnwls961@gmail.com>
2022-03-08 23:40:16 +0900

    solve 12933

--- 12933/12933/12933/12933.cpp
+++ 12933/12933/12933/12933.cpp
@@ -1,13 +1,31 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 long long solution(long long n) {
     long long answer = 0;
+    vector<int> vc;
+    int r=0;
 
+    while (n != 0)
+    {
+        r = n % 10;
+        n = n / 10;
+        vc.push_back(r);
+    }
 
+    sort(vc.begin(), vc.end());
+
+    for (int i = 0; i < vc.size(); i++)
+    {
+        cout << "vc is" << vc[i] << '\n';
+        answer += (vc[i] * (pow(10, i)));
+    }
+        
+    cout << "answer is" << answer << '\n';
 
     return answer;
 }
