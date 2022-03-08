5622f74254b92bf2d158f5c03601a6c1ba833fb5
firstTrip <hdnwls961@gmail.com>
2022-03-08 23:25:18 +0900

    Problem 12933
    https://programmers.co.kr/learn/courses/30/lessons/12933

---
+++ 12933/12933/12933/12933.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+
+
+
+    return answer;
+}
+
+int main()
+{
+    solution(118372);
+
+    return 0;
+}
