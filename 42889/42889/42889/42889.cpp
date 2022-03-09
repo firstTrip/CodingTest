8ca2e659e8c1d35b3f876a85aa6f0b7338844b91
firstTrip <hdnwls961@gmail.com>
2022-03-10 05:46:37 +0900

    Problem 42889

---
+++ 42889/42889/42889/42889.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    return answer;
+}
+
+int main()
+{
+
+
+    return 0;
+}
