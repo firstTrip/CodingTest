282c7fe6daf77aea0bbfcf85c4215087ec1eda23
firstTrip <hdnwls961@gmail.com>
2022-03-15 00:03:59 +0900

    solve 2506

--- baekjoon/2506/2506/2506/2506.cpp
+++ baekjoon/2506/2506/2506/2506.cpp
@@ -1,5 +1,38 @@
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int solution(vector<int> num) {
+   
+	int answer = 0;
+	int temp = 0;
+
+	for (int i = 0; i < num.size(); i++)
+	{
+		if (num[i] == 0)
+		{
+			temp = 0;
+			cout << "temp is " << temp << '\n';
+		}
+		else
+		{
+			temp++;
+			answer += temp;
+			cout << "temp is " << temp << '\n';
+		}
+	}
+	cout << answer;
+
+    return answer;
+}
+
 
 int main()
 {
+	vector<int> nums = { 1,0,1,1,1,0,0,1,1,0 };
+
+	solution(nums);
+	
 	return 0;
 }
