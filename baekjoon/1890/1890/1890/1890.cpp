628a1dcab678e248e7a9281b1a22746832b508a0
firstTrip <hdnwls961@gmail.com>
2022-04-01 00:25:47 +0900

    solving 1890

--- baekjoon/1890/1890/1890/1890.cpp
+++ baekjoon/1890/1890/1890/1890.cpp
@@ -2,8 +2,46 @@
 
 using namespace std;
 
+int n;
+int arr[101][101] = {-1,};
+int temp;
+int answer = 0;
+
+void Search(int x,int y)
+{
+	if (arr[x][y] == -1)
+		return;
+
+	if (arr[x][y] != 0)
+	{
+		Search(x + arr[x][y], y);
+		Search(x, y + arr[x][y]);
+	}
+	else if(arr[x][y] == 0)
+	{
+		answer++;
+	}
+
+}
+
 int main()
 {
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> temp;
+			arr[i][j] = temp;
+
+		}
+	}
+
+
+	Search(0, 0);
+
+	cout << answer;
 
 	return 0;
 }
