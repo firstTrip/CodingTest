1af969c917f2de797a75b3c37ea1174cc0d81096
firstTrip <hdnwls961@gmail.com>
2022-04-21 19:36:43 +0900

    solve 1018

--- baekjoon/1018/1018/1018/1018.cpp
+++ baekjoon/1018/1018/1018/1018.cpp
@@ -1,70 +1,79 @@
-#include<iostream>
-
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
 using namespace std;
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
 
-int N, M; //가로,세로 
+string board[51];
 
-char arr[50][50];
-int minCnt = 64;
-
-
-int MakeChes(int x,int y)
+int WB_cnt(int x, int y)
 {
-	int cnt = 0;
+    int cnt = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[x + i][y + j] != WB[i][j])
+                cnt++;
+        }
 
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			if (arr[x+i][y+j] == 'B')
-			{
-				if (arr[x + i + 1][y + j + 1] != 'W')
-				{
-					minCnt++;
-				}
-
-			}
-			else
-			{
-				if (arr[x + i + 1][y + j + 1] != 'B')
-				{
-					minCnt++;
-				}
-
-			}
-			j++;
-		}
-		i++;
-	}
-
-	return cnt;
+    }
+    return cnt;
 }
-
-int main()
+int BW_cnt(int x, int y)
 {
-	cin >> N >> M;
-	
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < M; j++)
-			cin >> arr[i][j];
-	}
+    int cnt = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[x + i][y + j] != BW[i][j])
+                cnt++;
+        }
 
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (N - i < 8|| M-j<8)
-				continue;
+    }
+    return cnt;
+}
+int main() {
 
-			if (minCnt < MakeChes(i, j))
-				minCnt = MakeChes(i, j);
-		}
-
-		
-	}
+    int min_val = 2500;
+    int N, M;
+    cin >> N >>M;
+    for (int i = 0; i < N; i++)
+        cin >> board[i];
 
 
-	cout << minCnt;
-
-	return 0;
+    for (int i = 0; i + 8 <= N; i++)
+    {
+        for (int j = 0; j + 8 <= M; j++)
+        {
+            int tmp;
+            tmp = min(WB_cnt(i, j), BW_cnt(i, j));
+            if (tmp < min_val) {
+                min_val = tmp;
+            }
+        }
+    }
+    cout << min_val;
+    return 0;
 }
