67695053e4a72f7b627cb35728a12a7f04f76779
firstTrip <hdnwls961@gmail.com>
2022-05-04 02:43:07 +0900

    commit

--- baekjoon/1193/1193/1193/1193.cpp
+++ baekjoon/1193/1193/1193/1193.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 #include<cmath>
+#include<vector>
 using namespace std;
 
 int n;
 float row;
+int cnt=1; // 행렬의 수
+
+
+int dp[1001];
+vector<pair<int, int>> v;
+
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -13,9 +20,11 @@ int main()
 
 	row = ceil(sqrt(n));
 
-	for (int i = 0; i < row; i++)
+	v.push_back(make_pair(1, 1));
+
+	while (true) 
 	{
-		for (int j = 0; j < row; j++)
+		while (cnt>v[cnt].first + v[cnt].second)
 		{
 
 		}
