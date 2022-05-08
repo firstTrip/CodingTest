c4e5bd8a2e51f27d1fb520aef081d77744d60983
firstTrip <hdnwls961@gmail.com>
2022-05-08 22:29:29 +0900

    problem 3085

---
+++ baekjoon/3085/3085/3085/3085.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int n;
+
+char arr[50][50];
+bool check[50][50];
+
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> arr[i][j];
+
+
+	
+
+
+
+	return 0;
+}
