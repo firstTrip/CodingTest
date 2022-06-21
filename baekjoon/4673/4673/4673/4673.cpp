2f4c182a9b9b1d880d6d8329543491164be58bcd
firstTrip <hdnwls961@gmail.com>
2022-06-21 19:02:39 +0900

    solve 4673

--- baekjoon/4673/4673/4673/4673.cpp
+++ baekjoon/4673/4673/4673/4673.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
 
+using namespace std;
+
+pair<int, bool> p[10001];
+
+void d(int n)
+{
+	int sum = n;
+
+	while (n != 0)
+	{
+		sum = sum + (n % 10);
+		n = n / 10;
+	}
+
+	p[sum].second = true;
+}
+
 int main()
 {
+	for (int i = 1; i < 10001; i++)
+	{
+		p[i].first = i;
+		p[i].second = false;
+	}
+
+	for (int i = 1; i < 10001; i++)
+	{
+		d(i);
+	}
+
+	for (int i = 1; i < 10001; i++)
+	{
+		if (!p[i].second)
+			cout << p[i].first << endl;
+	}
+
 	return 0;
 }
