beca00bbf716f98b7aa58f6ac49c095c16f4d0c9
firstTrip <hdnwls961@gmail.com>
2022-06-25 00:46:30 +0900

    bfs

---
+++ baekjoon/2644/2644/2644/2644.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int n,m;
+
+int x, y;
+
+int map[100][100];
+
+
+pair<int, int> p[100];
+bool visited[100] = { false, };
+
+void DFS(int port)
+{
+
+	if (!visited)
+	{
+		
+	}
+}
+
+int main()
+{
+	cin >> n;
+
+	cin >> x >> y;
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> p[i].first >> p[i].second;
+	}
+
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> p[i].first >> p[i].second;
+	}
+
+	return 0;
+}
---
+++ baekjoon/9372/9372/9372/9372.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int t;
+
+int arr[1000][10000];
+
+int main()
+{
+	cin >> t;
+
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+		int a, b;
+		while (m--)
+		{
+			cin >> a >> b;
+		}
+
+		cout << n - 1 << endl;
+	}
+
+	return 0;
+}
